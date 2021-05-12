140c3c7baa27ac067890710951298e4cb5d735bb
axis-mikeepascual <70327420+axis-mikeepascual@users.noreply.github.com>
2021-05-12 11:59:54 +0800

    Final product for login /plants.

--- src/public/index.php
+++ src/public/index.php
@@ -53,126 +53,109 @@ function isValidDate ($date_from, $date_to) {
     return true;
 }
 
-$app->post('/downloadplants', function () use ($app) {
+// $app->post('/downloadplants', function () use ($app) {
+//     $request = $app->request->post();
+
+//     $date_from = $request['date_from'];
+//     $date_to_raw = $request['date_to'];
+//     $date_to = date('Y-m-d', strtotime("+1 day", strtotime($date_to_raw)));
+
+//     $plant_uri = $request['plants'];
+
+//     $plant_uri_cleaned = substr($plant_uri, 0, -1);
+//     $plant_name = explode('https://ichigo.sp-viewer.net/', $plant_uri_cleaned);
+
+//     $csv_name = $plant_name[1];
+
+//     if (isValidDate($date_from, $date_to) != true) {
+//         $app->render('error.phtml');
+//         return;
+//     }
+
+//     $download_report_link = $plant_uri."ac/download_measurement.php?freq=daily&target_date_ge="
+//                             .$date_from."&target_date_lt=".$date_to."&format=csv&table="
+//                             .$csv_name."-".$date_from."-".$date_to_raw;
+    
+//     $app->redirect($download_report_link);
+// })->setName('downloadplants');
+
+// $app->get('/plants(/:stat)', function () use ($app) {
+
+//     $cookieJar = CookieJar::fromArray(['cookie_name' => 'cookie_value'], 'https://dev-integration.sp-viewer.net');
+//     $client = new \GuzzleHttp\Client(["base_uri" => "https://dev-integration.sp-viewer.net", 'cookies' => true]);
+
+//     $params = [
+//         "loginid" => "test",
+//         "password" => "A1X2I3S4"
+//     ];
+
+//     // 笠岡		http://180.178.84.94/index.html
+//     // 木城町		http://ichigo.delightviewer.jp/front/
+//     // 世羅津口		http://202.229.41.205/icg/pg/spv/h2/solar/front/
+//     // 世羅青水		http://202.229.41.205/icg/pg/spv/h3/solar/front/
+//     // 芽室西士狩	http://202.229.41.205/icg/pg/spv/h4/solar/front/
+//     // 常陸大宮		http://202.229.41.205/icg/pg/spv/h6/solar/front/
+//     // 世羅下津田	No site
+//     // 都城東霧島	No site
+//     // えびの末永	No site
+//     // 米子泉	No site
+    
+//     $plantsToSkip = array ('笠岡', '木城町', '世羅津口', '世羅青水', '芽室西士狩', '常陸大宮', '世羅下津田', '都城東霧島', 'えびの末永', '米子泉');
+
+//     $response = $client->post("/api/v1/login", ['json' => $params, 'cookies' => $cookieJar]);
+    
+//     $response = $client->get("/api/v1/plants", ['cookies' => $cookieJar]);
+//     $plantJsonDecoded = json_decode($response->getBody(), true);
+//     $plants = [];
+//     $plantsNonG = [];
+
+//     foreach ($plantJsonDecoded as $plant) {
+        
+//         if ($plant['plant_name'][0] == 'G') {
+//             $newPair = [ 
+//                 'plant_name' => $plant['plant_name'],
+//                 'plant_uri' => $plant['plant_uri'],
+//             ];
+//             array_push($plants, $newPair);
+//         }
+//         else {
+//             $matched = false;
+//             foreach ($plantsToSkip as $plantToSkip) {
+//                 if ($plant['plant_name'] == $plantToSkip) {
+//                     $matched = true;
+//                     break;
+//                 }
+//             }
+
+//             if ($matched == false) {
+//                 $newPair = [ 
+//                     'plant_name' => $plant['plant_name'],
+//                     'plant_uri' => $plant['plant_uri'],
+//                 ];
+//                 array_push($plantsNonG, $newPair);
+//             }
+//         }
+//     }
+
+//     $app->render('plants.phtml', ['plants' => $plants, 'plantsNonG' => $plantsNonG]);
+// })->setName('plants');
+
+$app->get('/login', function () use($app) {
+    $app->render('login.phtml');
+});
+
+$app->get('/plants(/:stat)', function () use($app) {
+    $app->render('login.phtml');
+});
+
+$app->post('/plants(/:stat)', function () use ($app) {
+
     $request = $app->request->post();
 
-    $date_from = $request['date_from'];
-    $date_to_raw = $request['date_to'];
-    $date_to = date('Y-m-d', strtotime("+1 day", strtotime($date_to_raw)));
-
-    $plant_uri = $request['plants'];
-
-    $plant_uri_cleaned = substr($plant_uri, 0, -1);
-    $plant_name = explode('https://ichigo.sp-viewer.net/', $plant_uri_cleaned);
-
-    $csv_name = $plant_name[1];
-
-    if (isValidDate($date_from, $date_to) != true) {
-        $app->render('error.phtml');
-        return;
-    }
-
-    $download_report_link = $plant_uri."ac/download_measurement.php?freq=daily&target_date_ge="
-                            .$date_from."&target_date_lt=".$date_to."&format=csv&table="
-                            .$csv_name."-".$date_from."-".$date_to_raw;
-    
-    $app->redirect($download_report_link);
-})->setName('downloadplants');
-
-$app->get('/plants(/:stat)', function () use ($app) {
-
-    $cookieJar = CookieJar::fromArray(['cookie_name' => 'cookie_value'], 'https://dev-integration.sp-viewer.net');
-    $client = new \GuzzleHttp\Client(["base_uri" => "https://dev-integration.sp-viewer.net", 'cookies' => true]);
-
-    $params = [
-        "loginid" => "test",
-        "password" => "A1X2I3S4"
-    ];
-
-    // 笠岡		http://180.178.84.94/index.html
-    // 木城町		http://ichigo.delightviewer.jp/front/
-    // 世羅津口		http://202.229.41.205/icg/pg/spv/h2/solar/front/
-    // 世羅青水		http://202.229.41.205/icg/pg/spv/h3/solar/front/
-    // 芽室西士狩	http://202.229.41.205/icg/pg/spv/h4/solar/front/
-    // 常陸大宮		http://202.229.41.205/icg/pg/spv/h6/solar/front/
-    // 世羅下津田	No site
-    // 都城東霧島	No site
-    // えびの末永	No site
-    // 米子泉	No site
-    
-    $plantsToSkip = array ('笠岡', '木城町', '世羅津口', '世羅青水', '芽室西士狩', '常陸大宮', '世羅下津田', '都城東霧島', 'えびの末永', '米子泉');
-
-    $response = $client->post("/api/v1/login", ['json' => $params, 'cookies' => $cookieJar]);
-    
-    $response = $client->get("/api/v1/plants", ['cookies' => $cookieJar]);
-    $plantJsonDecoded = json_decode($response->getBody(), true);
-    $plants = [];
-    $plantsNonG = [];
-
-    foreach ($plantJsonDecoded as $plant) {
+    if (isset($_POST['username'], $_POST['password'])) {
         
-        if ($plant['plant_name'][0] == 'G') {
-            $newPair = [ 
-                'plant_name' => $plant['plant_name'],
-                'plant_uri' => $plant['plant_uri'],
-            ];
-            array_push($plants, $newPair);
-        }
-        else {
-            $matched = false;
-            foreach ($plantsToSkip as $plantToSkip) {
-                if ($plant['plant_name'] == $plantToSkip) {
-                    $matched = true;
-                    break;
-                }
-            }
-
-            if ($matched == false) {
-                $newPair = [ 
-                    'plant_name' => $plant['plant_name'],
-                    'plant_uri' => $plant['plant_uri'],
-                ];
-                array_push($plantsNonG, $newPair);
-            }
-        }
-    }
-
-    $app->render('plants.phtml', ['plants' => $plants, 'plantsNonG' => $plantsNonG]);
-})->setName('plants');
-
-$app->get('/test/login', function () use($app){
-
-//    $request = $app->request->get();
-
-if (isset($_GET['username'], $_GET['password'])) {
-    echo $_GET['username'];
-    echo $_GET['password'];
-} else {
-    echo '';
-}
-
-    $app->render('test2.phtml');
-});
-
-$app->post('/test/new', function () use($app){
-
-    //$request = $app->request->post();
-
-    $uname = $_GET['username'];
-    $pass = $_GET['password'];
-
-    echo ($uname);
-    echo ($pass);
-    $app->render('test2.phtml');
-
-});
-
-$app->get('/test(/:stat)', function () use ($app) {
-
-    if (isset($_GET['username'], $_GET['password'])) {
-        
-        $username = $_GET['username'];
-        $password = $_GET['password'];
+        $username = $_POST['username'];
+        $password = $_POST['password'];
         
         if ($username == "test" && $password == "A1X2I3S4") {
             $cookieJar = CookieJar::fromArray(['cookie_name' => 'cookie_value'], 'https://dev-integration.sp-viewer.net');
@@ -231,7 +214,7 @@ $app->get('/test(/:stat)', function () use ($app) {
                 }
             }
 
-            $app->render('test.phtml', ['plants' => $plants, 'plantsNonG' => $plantsNonG]);
+            $app->render('plants.phtml', ['plants' => $plants, 'plantsNonG' => $plantsNonG]);
         }
         else {
             return $app->render('error.phtml');
